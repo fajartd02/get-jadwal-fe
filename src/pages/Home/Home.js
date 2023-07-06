34e9a7a7eaf2bfc54090feb717778a1aea043620
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-07-06 21:22:25 +0700

    intergrate home

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,11 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import NavbarCustom from "../../components/Navbar";
 import { Button, Col, Container, Row } from "react-bootstrap";
 import { pinkBg } from "../../constant";
 import axios from "axios";
 import CardCustom from "../../components/CardCustom";
+import ModalCreate from "../../components/ModalCreate";
 
 function Home() {
   const [monday, setMonday] = useState([]);
@@ -11,31 +13,61 @@ function Home() {
   const [wednesday, setWednesday] = useState([]);
   const [thursday, setThursday] = useState([]);
   const [friday, setFriday] = useState([]);
+  const [modalShow, setModalShow] = React.useState(false);
 
-  const fetchSchedule = async () => {
+  const getSchedules = async () => {
     const email = localStorage.getItem("email");
     try {
-      const response = await axios.get(
-        "https://getjadwal.api.devcode.gethired.id/schedule?email=" + email
+      let response = await axios.get(
+        `https://getjadwal.api.devcode.gethired.id/schedule?email=${email}&day=monday`
       );
-      console.log(response.data);
+      setMonday(response.data.data);
+
+      response = await axios.get(
+        `https://getjadwal.api.devcode.gethired.id/schedule?email=${email}&day=tuesday`
+      );
+      setTuesday(response.data.data);
+
+      response = await axios.get(
+        `https://getjadwal.api.devcode.gethired.id/schedule?email=${email}&day=wednesday`
+      );
+      setWednesday(response.data.data);
+
+      response = await axios.get(
+        `https://getjadwal.api.devcode.gethired.id/schedule?email=${email}&day=thursday`
+      );
+      setThursday(response.data.data);
+
+      response = await axios.get(
+        `https://getjadwal.api.devcode.gethired.id/schedule?email=${email}&day=friday`
+      );
+      setFriday(response.data.data);
     } catch (err) {
       console.error(err);
     }
   };
 
+  const handleFunctionClose = () => {
+    setModalShow(false);
+  };
+
   useEffect(() => {
-    fetchSchedule();
+    getSchedules();
   }, []);
 
   return (
     <>
       <NavbarCustom />
+      <ModalCreate
+        modalShow={modalShow}
+        handleFunctionClose={handleFunctionClose}
+      />
       <Container>
         <Button
           style={{ background: pinkBg, float: "right" }}
           className="rounded mt-4"
           data-cy="btn-create-schedule"
+          onClick={() => setModalShow(true)}
         >
           + Buat Jadwal Kuliah
         </Button>
@@ -45,19 +77,19 @@ function Home() {
 
         <Row style={{ marginTop: "20px" }}>
           <Col>
-            <CardCustom day={"Senin"} />
+            <CardCustom day={"Senin"} data={monday} />
           </Col>
           <Col>
-            <CardCustom day={"Selasa"} />
+            <CardCustom day={"Selasa"} data={tuesday} />
           </Col>
           <Col>
-            <CardCustom day={"Rabu"} />
+            <CardCustom day={"Rabu"} data={wednesday} />
           </Col>
           <Col>
-            <CardCustom day={"Senin"} />
+            <CardCustom day={"Senin"} data={thursday} />
           </Col>
           <Col>
-            <CardCustom day={"Senin"} />
+            <CardCustom day={"Senin"} data={friday} />
           </Col>
         </Row>
       </Container>
