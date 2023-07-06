e37ca099a398c660b91ac158ddd52313bd043ef7
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-07-06 19:02:56 +0700

    image validation

--- src/pages/CheckIn/CheckIn.js
+++ src/pages/CheckIn/CheckIn.js
@@ -1,9 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import NavbarCustom from "../../components/Navbar";
 import { Button, Card, Container, Form } from "react-bootstrap";
 import { pinkBg } from "../../constant";
+import { validate as IsEmail } from "email-validator";
 
 function CheckIn() {
+  const [email, setEmail] = useState("");
+  const [isValid, setIsValid] = useState(false);
+  const [firstReload, setFirstReload] = useState(true);
+
+  const handleEmailChange = (e) => {
+    const inputValue = e.target.value;
+    setEmail(inputValue);
+    setIsValid(IsEmail(inputValue));
+    setFirstReload(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Perform desired action when the form is submitted
+    console.log("Submitted email:", email);
+  };
+
   return (
     <>
       <NavbarCustom />
@@ -15,7 +33,6 @@ function CheckIn() {
           >
             Check In
           </h4>
-
           <p
             style={{
               marginLeft: "60px",
@@ -36,11 +53,29 @@ function CheckIn() {
             }}
             placeholder="Masukkan email anda"
             data-cy="input-email"
+            onChange={handleEmailChange}
           />
+
+          {!isValid && !firstReload && (
+            <p style={{ marginLeft: "60px", color: "red" }}>
+              <img
+                src="/img/warning.png"
+                alt="test"
+                style={{ marginRight: "8px" }}
+              />
+              Format email tidak sesuai
+            </p>
+          )}
           <Button
             className="w-75 m-auto mt-1 mb-4"
-            style={{ backgroundColor: pinkBg, fontWeight: "bold" }}
+            style={{
+              backgroundColor: pinkBg,
+              fontWeight: "bold",
+              opacity: isValid ? 1 : 0.3,
+            }}
             data-cy="btn-login"
+            disabled={!isValid}
+            onClick={handleSubmit}
           >
             Mulai Sesi
           </Button>
