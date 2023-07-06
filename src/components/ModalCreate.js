7c3bab24a94d69ff466cd1aa6ac7d74ef26c01f0
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-07-06 21:41:42 +0700

    modal craete be integrate

--- src/components/ModalCreate.js
+++ src/components/ModalCreate.js
@@ -1,11 +1,34 @@
 import React, { useState } from "react";
 import { Button, Modal, CloseButton, Container, Form } from "react-bootstrap";
 import { pinkBg } from "../constant";
+import axios from "axios";
 
 function ModalCreate(props) {
-  const { modalShow, handleFunctionClose } = props;
+  const { modalShow, handleFunctionClose, handlePostDataInCreate } = props;
+  const [title, setTitle] = useState("");
+  const [day, setDay] = useState("");
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+
+  const handleDayChange = (e) => {
+    setDay(e.target.value);
+  };
   const handleSubmit = async () => {
-    console.log("MASOK");
+    const email = localStorage.getItem("email");
+    const body = { title: title, day: day };
+    try {
+      await axios.post(
+        "https://getjadwal.api.devcode.gethired.id/schedule?email=" + email,
+        body
+      );
+      handleFunctionClose();
+      handlePostDataInCreate();
+      setTitle("");
+      setDay("");
+    } catch (err) {
+      console.error(err);
+    }
   };
   return (
     <>
@@ -28,20 +51,29 @@ function ModalCreate(props) {
             <Form.Control
               type="text"
               id="inputMataKuliah"
-              data-cy="form-matkul"
               placeholder="Masukkan Mata Kuliah"
+              value={title}
+              onChange={handleTitleChange}
+              data-cy="form-matkul"
             />
 
             <Form.Label htmlFor="inputPilihHari" className="mt-2">
               Pilih Hari
             </Form.Label>
-            <Form.Select id="inputPilihHari" data-cy="form-day">
-              <option value="" disabled selected>
+            <Form.Select
+              id="inputPilihHari"
+              data-cy="form-day"
+              value={day}
+              onChange={handleDayChange}
+            >
+              <option value="" disabled>
                 Pilih Hari
               </option>
-              <option value="1">One</option>
-              <option value="2">Two</option>
-              <option value="3">Three</option>
+              <option value="monday">Senin</option>
+              <option value="tuesday">Selasa</option>
+              <option value="wednesday">Rabu</option>
+              <option value="thursday">Kamis</option>
+              <option value="friday">Jumat</option>
             </Form.Select>
           </Modal.Body>
           <Modal.Footer>
