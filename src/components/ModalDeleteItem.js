de5c344c455ffde497e32336a88554b5ba29f916
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-07-07 14:53:33 +0700

    testing

--- src/components/ModalDeleteItem.js
+++ src/components/ModalDeleteItem.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/alt-text */
 import axios from "axios";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, CloseButton, Container, Modal } from "react-bootstrap";
 
 function ModalDeleteItem(props) {
@@ -11,6 +11,14 @@ function ModalDeleteItem(props) {
     handleFunctionCloseDelete,
     handleDeleteData,
   } = props;
+
+  const [isModalContentVisible, setModalContentVisible] = useState(false);
+
+  useEffect(() => {
+    if (modalShow) {
+      setModalContentVisible(true);
+    }
+  }, [modalShow]);
 
   const handleDelete = async () => {
     const email = localStorage.getItem("email");
@@ -36,53 +44,59 @@ function ModalDeleteItem(props) {
         centered
         data-cy="form-delete"
         className="rounded"
+        onEntered={() => setModalContentVisible(true)}
+        onHide={() => setModalContentVisible(false)}
       >
         <CloseButton
           data-cy="close-modal"
           style={{ position: "absolute", right: 10, top: 10 }}
           onClick={handleFunctionCloseDelete}
         />
-        <img
-          src="/img/delete.png"
-          width={88}
-          height={88}
-          style={{ margin: "auto", marginTop: "24px" }}
-        />
-        <Container className="mt-2 mb-2">
-          <h2 className="text-center" style={{ fontWeight: "bold" }}>
-            Hapus Mata Kuliah
-          </h2>
-          <p className="text-center">
-            Apakah anda yakin menghapus kuliah {selectedTitle}?
-          </p>
-        </Container>
-        <div style={{ margin: "auto" }} className="mb-4">
-          <Button
-            id="btn-batal"
-            className="btn"
-            style={{
-              backgroundColor: "#F4F4F4",
-              color: "#888888",
-              fontWeight: "bold",
-              width: "112px",
-              marginRight: "10px",
-            }}
-            data-cy="btn-close"
-            onClick={() => {
-              handleFunctionCloseDelete();
-            }}
-          >
-            Batal
-          </Button>
-          <Button
-            className="btn btn-danger"
-            style={{ width: "112px" }}
-            onClick={handleDelete}
-            data-cy="btn-submit"
-          >
-            Hapus
-          </Button>
-        </div>
+        {isModalContentVisible && (
+          <>
+            <img
+              src="/img/delete.png"
+              width={88}
+              height={88}
+              style={{ margin: "auto", marginTop: "24px" }}
+            />
+            <Container className="mt-2 mb-2">
+              <h2 className="text-center" style={{ fontWeight: "bold" }}>
+                Hapus Mata Kuliah
+              </h2>
+              <p className="text-center">
+                Apakah anda yakin menghapus kuliah {selectedTitle}?
+              </p>
+            </Container>
+            <div style={{ margin: "auto" }} className="mb-4">
+              <Button
+                id="btn-batal"
+                className="btn"
+                style={{
+                  backgroundColor: "#F4F4F4",
+                  color: "#888888",
+                  fontWeight: "bold",
+                  width: "112px",
+                  marginRight: "10px",
+                }}
+                data-cy="btn-close"
+                onClick={() => {
+                  handleFunctionCloseDelete();
+                }}
+              >
+                Batal
+              </Button>
+              <Button
+                className="btn btn-danger"
+                style={{ width: "112px" }}
+                onClick={handleDelete}
+                data-cy="btn-submit"
+              >
+                Hapus
+              </Button>
+            </div>
+          </>
+        )}
       </Modal>
     </>
   );
