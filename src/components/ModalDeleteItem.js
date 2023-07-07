8799a63c6943e2a3c30062780674040a84c69f0b
Fajar Muhammad Hamka <fajarmh2@gmail.com>
2023-07-07 14:20:31 +0700

    use form

--- src/components/ModalDeleteItem.js
+++ src/components/ModalDeleteItem.js
@@ -30,62 +30,60 @@ function ModalDeleteItem(props) {
 
   return (
     <>
-      <Modal
-        show={modalShow}
-        size="lg"
-        centered
-        data-cy="form-delete"
-        className="rounded"
-        style={{ display: "block" }}
-      >
-        <img
-          src="/img/delete.png"
-          width={88}
-          height={88}
-          style={{ margin: "auto", marginTop: "24px" }}
-        />
-        <Modal.Body style={{ margin: "auto" }}>
-          <Container>
-            <h2 className="text-center" style={{ fontWeight: "bold" }}>
-              Hapus Mata Kuliah
-            </h2>
-            <p className="text-center">
-              Apakah anda yakin menghapus kuliah {selectedTitle}?
-            </p>
-          </Container>
-        </Modal.Body>
-        <Modal.Footer style={{ margin: "auto" }}>
-          <Row>
-            <Col>
-              <Button
-                id="btn-batal"
-                className="btn"
-                style={{
-                  backgroundColor: "#F4F4F4",
-                  color: "#888888",
-                  fontWeight: "bold",
-                  width: "112px",
-                }}
-                data-cy="btn-close"
-                onClick={() => {
-                  handleFunctionCloseDelete();
-                }}
-              >
-                Batal
-              </Button>
-            </Col>
-            <Col>
-              <Button
-                className="btn btn-danger"
-                style={{ width: "112px" }}
-                onClick={handleDelete}
-                data-cy="btn-submit"
-              >
-                Hapus
-              </Button>
-            </Col>
-          </Row>
-        </Modal.Footer>
+      <Modal show={modalShow} size="lg" centered className="rounded">
+        <form
+          data-cy="form-delete"
+          className="d-flex flex-column align-items-center"
+        >
+          <img
+            src="/img/delete.png"
+            width={88}
+            height={88}
+            style={{ position: "relative", marginTop: "24px" }}
+          />
+          <Modal.Body style={{ margin: "auto" }}>
+            <Container>
+              <h2 className="text-center" style={{ fontWeight: "bold" }}>
+                Hapus Mata Kuliah
+              </h2>
+              <p className="text-center">
+                Apakah anda yakin menghapus kuliah {selectedTitle}?
+              </p>
+            </Container>
+          </Modal.Body>
+          <Modal.Footer style={{ margin: "auto" }}>
+            <Row>
+              <Col>
+                <Button
+                  id="btn-batal"
+                  className="btn"
+                  style={{
+                    backgroundColor: "#F4F4F4",
+                    color: "#888888",
+                    fontWeight: "bold",
+                    width: "112px",
+                  }}
+                  data-cy="btn-close"
+                  onClick={() => {
+                    handleFunctionCloseDelete();
+                  }}
+                >
+                  Batal
+                </Button>
+              </Col>
+              <Col>
+                <Button
+                  className="btn btn-danger"
+                  style={{ width: "112px" }}
+                  onClick={handleDelete}
+                  data-cy="btn-submit"
+                >
+                  Hapus
+                </Button>
+              </Col>
+            </Row>
+          </Modal.Footer>
+        </form>
       </Modal>
     </>
   );
